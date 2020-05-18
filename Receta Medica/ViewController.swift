fdf94b4a61123ac68a71f68791d38e68deb79cc1
Jorge Collins <jorge.collins@gmail.com>
2020-05-18 00:25:17 -0600

    Push Notifications: sin cambios, solo seleccione el proyecto (primer elemento en el árbol) y le di commit palomeando todos los archivos.

---
+++ Receta Medica/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Receta Medica
+//
+//  Created by Jorge Collins Gómez on 17/05/20.
+//  Copyright © 2020 Jorge Collins Gómez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
